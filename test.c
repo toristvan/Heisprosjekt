50a87ed503903eb0b23f856e15591dccec98ed73
erlenddahlen <37074633+erlenddahlen@users.noreply.github.com>
2018-03-05 17:40:45 +0100

    Update test.c

--- test.c
+++ test.c
@@ -70,30 +70,40 @@ bool checkTimerFinished(){
 
 // TIMER END 
 main(){
-//init
 
+elev_Init();
 
 while(1){
-if(elev_get_button_signal){
-	new_order(order)
+
+if(elev_get_button_signal()){
+	new_order();
 }
-if(orderfinished()){
+if(elev_get_floor_sensor_signal()>-1){
+	currentfloor = 	elev_get_floor_sensor_signal()+1;
+	elev_set_floor_indicator()
+}
+	
+if(orderfinished()){ 
 	remove_order();
 	elev_set_motor_direction(Queue[0].dir);
 }
 if (elev_get_stop_signal()) {
-            elev_set_motor_direction(DIRN_STOP);
+        elev_set_motor_direction(DIRN_STOP);
+	queueInit();
+	   
             break;
 }
 if(checktimer()){
+	elev_set_door_open_lamp(0);
 	executeorder();
 }
 
-if(Queue[0].etasjestopp[currentfloor]){
-	 elev_set_motor_direction(DIRN_STOP);
-	//settimer
+if(Queue[0].etasjestopp[currentfloor-1] || (MOTORDIR == DIRN_STOP && elev_get_obstruction_signal())){
+	 stopElev();
 	}
 }
+
+
 
 
 

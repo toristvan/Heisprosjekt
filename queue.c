8e95476497a2aae99ec1c64dad9eb88eebb557f4
toristvan <25774883+toristvan@users.noreply.github.com>
2018-03-06 12:12:39 +0100

    Add files via upload

--- queue.c
+++ queue.c
@@ -1,10 +1,25 @@
 #include "queue.h"
 
+int orderEqual(struct order_type o1, struct order_type o2){
+  if (o1.dir==o2.dir && o1.floor==o2.floor){
+    return 1;
+  } else{
+    return 0;
+  }
+}
+
+int queueEqual(struct internal_order q1,struct internal_order q2){
+  if (orderEqual(q1.order,q2.order) && q1.etasjestopp==q2.etasjestopp){//evt valid.
+    return 1;
+  } else{
+    return 0;
+  }
+}
 
 void queueInit()
 {
   for(int i=0;i<sizeof(queue);i++){
-  queue[i].order.dir=0; queue[i].order.floor=0;queue[i].valid=0;queue[i].etasjestopp=[0,0,0,0];
+  queue[i].order.dir=0; queue[i].order.floor=0;queue[i].valid=0;queue[i].etasjestopp={0};
   }
 }
 void addExternalOrder(struct order_type neworder)
@@ -16,7 +31,7 @@ void addExternalOrder(struct order_type neworder)
       queue[i].valid=1;
       return;
     }
-    if(neworder.dir==queue[i].order.dir && neworder.floor==queue[i].order.floor){
+    if(neworder.dir==queue[i].order.dir && neworder.floor==queue[i].order.floor){ //orderEqual(neworder,queue[i])
       return;
     }
     if (neworder.dir==queue[i].order.dir){
@@ -33,10 +48,10 @@ void addExternalOrder(struct order_type neworder)
 }
 
 void addInternalOrder(int floor) {
-  if (queue[0].dir==DOWN && floor>=currentFloor) {//assert
+  if (queue[0].order.dir==DOWN && floor>=currentFloor) {//assert
     return;
   }
-  if (queue[0].dir==UP && floor<=currentFloor){
+  if (queue[0].order.dir==UP && floor<=currentFloor){
     return;
   }
   queue[0].etasjestopp[floor-1]=1;
@@ -49,17 +64,19 @@ int orderFinished()
 
 void removeOrder(int index)
 {
+  int j=0;
   for (int i=index+1;i<sizeof(queue);i++){
     queue[i-1]=queue[i];
+    j=i;
   }
-  queue[i].order.dir=NONE; queue[i].order.floor=0;queue[i].valid=0;
+  queue[j].order.dir=NONE; queue[j].order.floor=0;queue[j].valid=0;queue[j].etasjestopp={0};
 }
 
 
 void executeOrder()
 {
-  if(queue[0].etasjestopp[currentFloor-1]){
-    elev_set_motor_direction(queue[0].order.dir)
+  if(queue[0].etasjestopp[currentFloor-1]){ //endre order_dir_t til elev_motor_direction_t
+    elev_set_motor_direction(queue[0].order.dir);
   }
   else{
     for (int i=0; i<4;i++){
@@ -97,9 +114,9 @@ void newOrder()
 {    //tar utgangspunkt i at kun én knapp trykkes inn om gangen.
   struct order_type new_order;
   for (int floor=0;floor<4;floor++){
-    for (int button=0;button<3;button++){
-      if(button_channel_matrix[floor][button]){
-        io_set_bit(lamp_channel_matrix[floor][button])
+    for (elev_button_type_t button=BUTTON_CALL_UP;button<=BUTTON_COMMAND;button++){
+      if(elev_get_button_signal(button,floor)){
+        elev_set_button_lamp(button,floor,1);
         new_order.floor=floor+1;
         if(button==0){
           new_order.dir=UP;
@@ -122,11 +139,11 @@ void newOrder()
 void optimizeQueue()
 { //legg bare til i queue[0]
   for (int i=1;i<sizeof(queue);i++){
-    if(queue[0]==queue[i]){
+    if(queueEqual(queue[0],queue[i])){
       break;
     }
     if (queue[0].order.dir==queue[i].order.dir){
-      if (queue[i].order.dir==UP && queue[i].order.floor<=queue[0].order.floor &&currentFloor<queue[i].floor){
+      if (queue[i].order.dir==UP && queue[i].order.floor<=queue[0].order.floor &&currentFloor<queue[i].order.floor){
                   queue[0].etasjestopp[queue[i].order.floor-1]=1;
           return;
         }

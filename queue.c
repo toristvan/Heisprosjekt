15bbc64c6bd9138ab1d01f6fed5d5e1f34a029b6
erlenddahlen <37074633+erlenddahlen@users.noreply.github.com>
2018-03-06 10:51:51 +0100

    Update queue.c

--- queue.c
+++ queue.c
@@ -181,3 +181,22 @@ void optimizeQueue(){ //legg bare til i queue[0]
     }
   }
 }
+
+double get_time(void){
+	struct timeval time;
+	gettimeofday(&time, NULL);
+	return (double)time.tv_sec + (double)time.tv_usec*.000001;
+}
+static int run;
+static double endtime;
+
+void startTimer(double dur){
+	endtime=get_time()+dur;
+}
+
+int checkTimerFinished(){
+	if(get_time()<endtime){
+	return 0;
+	}
+	return 1;
+}  

8ed04b12e9acbac743896cc759db5b6a9acd00fe
toristvan <25774883+toristvan@users.noreply.github.com>
2018-03-11 19:38:48 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main() {
         printf("Unable to initialize elevator hardware!\n");
         return 1;
     }
-    printf("Press STOP button to stop elevator and exit program.\n");
+    printf("Press Ctrl+Z to exit program. PLease make sure elevator is stopped first.\n");
     while(elev_get_floor_sensor_signal()!=0){
       elev_set_motor_direction(DIRN_DOWN);
     }
@@ -16,7 +16,7 @@ int main() {
     //initializing variables.
     currentFloor=1;
     prevDir=DIRN_STOP;
-    //The main elevetaor sequence begins here.
+    //The main elevator sequence begins here.
     while (1) {
         //contionously updating currentFloor and floor indicators.
         updateFloor();
@@ -35,12 +35,14 @@ int main() {
             elev_set_door_open_lamp(0);
             executeOrder();
         }
+
         //stopping elevator when ordered floor is reached, and reactivates in case of obstruction.
         if(((queue[0].floorstop[currentFloor-1] && prevDir==DIRN_STOP)
             ||(elev_get_obstruction_signal() && !checkTimerFinished())) && floorValid>-1){
             stopElev();
             optimizeQueue();
         }
+
         //checking if timer and order is finished. If so removes order and optimizes queue.
         if(orderFinished() && queue[0].valid && checkTimerFinished()){
             removeOrder(0);
